523fd00f6cf99ecd27e8b62aebe294b592f13f17
xiaoningning <xiaoningning@yahoo.com>
2012-07-15 21:40:45 -0700

    longest string without repeatting

---
+++ PuzzleCoding/src/LongestStringWithoutRepeat.java
@@ -0,0 +1,48 @@
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ * For example, the longest substring without repeating letters for "abcabcbb" is "abc",
+ * which the length is 3.
+ * For "bbbbb" the longest substring is "b", with the length of 1.
+ *
+ */
+public class LongestStringWithoutRepeat {
+    public static void main(String[] args){
+        String s1 = "abcabcbb";
+        System.out.println(longestStringWithoutRepeat(s1));
+        String s2 = "bbb";
+        System.out.println(longestStringWithoutRepeat(s2));
+
+    }
+    public static String longestStringWithoutRepeat(String s){
+
+        boolean[] exist = new boolean[256];
+        int start = 0, j =0, maxStart=0, maxLen = 0, n = s.length();
+
+        while (j<n){
+            if(exist[s.charAt(j)]){
+                if(j-start > maxLen){
+                    maxLen = j-start;
+                    maxStart = start;
+                }
+
+                while (s.charAt(start) != s.charAt(j)){
+                    exist[s.charAt(start)] = false;
+                    start++;
+                }
+                j++;
+                start++;
+            }else {
+                exist[s.charAt(j)]=true;
+                j++;
+            }
+        }
+
+        if(n-start > maxLen){
+            maxLen = n-start;
+            maxStart = start;
+        }
+
+        return s.substring(maxStart, maxStart+maxLen);
+
+    }
+}

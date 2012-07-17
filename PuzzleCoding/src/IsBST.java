cd8a6c41fddf40d9c125ec071ed8cb759a01c46c
xiaoningning <xiaoningning@yahoo.com>
2012-07-16 17:57:19 -0700

    is bst

---
+++ PuzzleCoding/src/IsBST.java
@@ -0,0 +1,54 @@
+/**
+ * determine if a given binary tree is a binary search tree.
+ */
+public class IsBST {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        Node node5 = new Node(5);
+        Node node15 = new Node(15);
+        Node node6 = new Node(6);
+        Node node20 = new Node(20);
+        Node node3 = new Node(3);
+        Node node12 = new Node(12);
+
+        root.insert(node5);
+        root.insert(node6);
+        root.insert(node15);
+        root.insert(node20);
+        root.insert(node3);
+        root.insert(node12);
+
+        System.out.println(isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE));
+        System.out.println(isBST1(root, Integer.MIN_VALUE));
+
+    }
+
+    public static boolean isBST(Node node, int min, int max) {
+        if ((node.left == null) && (node.right == null)){
+            return true;
+        }
+        if ((min < node.value) && (node.value < max)) {
+            return isBST(node.left, min, node.value) &&
+                    isBST(node.right, node.value, max);
+        } else{
+            return false;
+        }
+
+    }
+
+    public static boolean isBST1(Node node, int prev) {
+        if ((node.left == null) && (node.right == null)) {
+            return true;
+        }
+        if (isBST1(node.left, prev)) {
+            if (node.value > prev) {
+                prev = node.value;
+                return isBST1(node.right, prev);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}

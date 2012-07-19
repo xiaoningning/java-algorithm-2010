09ddf68690f7cdc3459716df234b4b46ba5f1582
xiaoningning <xiaoningning@yahoo.com>
2012-07-18 17:54:48 -0700

    update in place bst to double linked list

--- PuzzleCoding/src/BSTtoDLL.java
+++ PuzzleCoding/src/BSTtoDLL.java
@@ -2,108 +2,142 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 /**
- * Convert a BST to a sorted circular doubly-linked list in-place.
- * Think of the left and right pointers as synonymous to the previous
- * and next pointers in a doubly-linked list.
+ * Convert a BST to a sorted circular doubly-linked list in-place. Think of the
+ * left and right pointers as synonymous to the previous and next pointers in a
+ * doubly-linked list.
  * <p/>
- * This is a modified in-order traversal adapted to this problem.
- * prev (init to NULL) is used to keep track of previously traversed node.
- * head pointer is updated with the list's head as recursion ends.
+ * This is a modified in-order traversal adapted to this problem. prev (init to
+ * NULL) is used to keep track of previously traversed node. head pointer is
+ * updated with the list's head as recursion ends.
  * <p/>
- * do it in place.  time complexity: O(n)
+ * do it in place. time complexity: O(n)
  */
 public class BSTtoDLL {
-    public static void main(String[] args) {
-        Node root = new Node(10);
-        Node node5 = new Node(5);
-        Node node15 = new Node(15);
-        Node node6 = new Node(6);
-        Node node20 = new Node(20);
-        Node node3 = new Node(3);
-        Node node12 = new Node(12);
-
-        root.insert(node5);
-        root.insert(node6);
-        root.insert(node15);
-        root.insert(node20);
-        root.insert(node3);
-        root.insert(node12);
-
-        ArrayList<Node> dll = bstToDLL(root);
-        printDll(dll);
-
-        bstToDll1(root);
-
-        System.out.print(tail.value);
-
-    }
+	public static void main(String[] args) {
+		Node root = new Node(10);
+		Node node5 = new Node(5);
+		Node node15 = new Node(15);
+		Node node6 = new Node(6);
+		Node node20 = new Node(20);
+		Node node3 = new Node(3);
+		Node node12 = new Node(12);
+		Node node14 = new Node(14);
 
 
-    public static void printDll(ArrayList<Node> dll) {
-        if (dll.size() != 0) {
-            for (Node n : dll) {
-                System.out.print(n.value + " ");
-            }
-        }
-        System.out.println();
-    }
+		root.insert(node5);
+		root.insert(node6);
+		root.insert(node15);
+		root.insert(node20);
+		root.insert(node3);
+		root.insert(node12);
+		root.insert(node14);
 
-    // not in place
-    public static ArrayList<Node> bstToDLL(Node node) {
-        if (node == null)
-            return null;
+		ArrayList<Node> dll = bstToDLL(root);
+		printDll(dll);
 
-        Stack<Node> stack = new Stack<Node>();
-        ArrayList<Node> dll = new ArrayList<Node>();
-        Node current = node;
-        boolean done = false;
-        while (!done) {
-            if (current != null) {
-                stack.push(current);
-                current = current.left;
-            } else {
-                if (stack.isEmpty()) {
-                    done = true;
-                } else {
-                    current = stack.pop();
-                    dll.add(current);
-                    current = current.right;
-                }
-            }
-        }
-        return dll;
-    }
+		bstToDllinPlace(root);
 
-    static Node prev, tail;
+		printDll(head);
 
-    public static void bstToDll1(Node p) {
-        if (p == null)
-            return;
+	}
 
-        bstToDll1(p.left);
-        p.left = prev;
-        // current node (smallest element) is head of
-        // the list if previous node is not available
-        if (prev == null){
-            tail = p;
-        }
-        else{
+	public static void printDll(Node n) {
+		if (n == null) {
+			System.out.println("null");
+		} else {
+			int s = n.value;
+			System.out.print(n.value + " ");
+			n = n.right;
+			while (n.value != s) {
+				System.out.print(n.value + " ");
+				n = n.right;
+			}
+		}
+		System.out.println();
+	}
 
-            prev.right = p;
-        }
+	public static void printDll(ArrayList<Node> dll) {
+		if (dll.size() != 0) {
+			for (Node n : dll) {
+				System.out.print(n.value + " ");
+			}
+		}
+		System.out.println();
+	}
 
-        // as soon as the recursion ends, the head's left pointer
-        // points to the last node, and the last node's right pointer
-        // points to the head pointer.
+	// not in place
+	public static ArrayList<Node> bstToDLL(Node node) {
+		if (node == null)
+			return null;
 
-        Node rightNode = p.right;
-        tail.left = p;
-        p.right = tail;
-        prev = p;
+		Stack<Node> stack = new Stack<Node>();
+		ArrayList<Node> dll = new ArrayList<Node>();
+		Node current = node;
+		boolean done = false;
+		while (!done) {
+			if (current != null) {
+				stack.push(current);
+				current = current.left;
+			} else {
+				if (stack.isEmpty()) {
+					done = true;
+				} else {
+					current = stack.pop();
+					dll.add(current);
+					current = current.right;
+				}
+			}
+		}
+		return dll;
+	}
 
-        bstToDll1(rightNode);
+	static Node prev, head;
 
-    }
+	public static void bstToDllinPlace(Node p) {
+		if (p == null)
+			return;
 
+		bstToDllinPlace(p.left);
+		p.left = prev;
+		// current node (smallest element) is head of
+		// the list if previous node is not available
+		if (prev == null) {
+			head = p;
+		} else {
+
+			prev.right = p;
+		}
+		System.out.print("parent: " + p.value + " prev: ");
+		nodeToString(prev);
+		System.out.print(" head: ");
+		nodeToString(head);
+		
+		// as soon as the recursion ends, the head's left pointer
+		// points to the last node, and the last node's right pointer
+		// points to the head pointer.
+
+		Node rightNode = p.right;
+		head.left = p;
+		p.right = head;
+		prev = p;
+
+		bstToDllinPlace(rightNode);
+
+		System.out.print("parent: " + p.value + " prev: ");
+		nodeToString(prev);
+		System.out.print(" head: ");
+		nodeToString(head);
+
+	}
+
+	public static void nodeToString(Node n) {
+		if (n == null) {
+			System.out.println("null");
+
+		} else {
+			System.out.println(n.value);
+		}
+
+	}
 
 }
---
+++ PuzzleCoding/src/Node.java
@@ -0,0 +1,41 @@
+/**
+ * a node class for tree
+ */
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int v){
+        value = v;
+        left = null;
+        right = null;
+
+    }
+    public boolean insert(Node n){
+        if(n == null)
+            return false;
+        if(value >= n.value){
+            if(left == null)
+                left = n;
+            else
+                left.insert(n);
+        }else {
+            if(right == null)
+                right = n;
+            else
+                right.insert(n);
+        }
+        return true;
+    }
+    public int size(){
+        int size = 0;
+        if(left != null){
+            size += left.size();
+        }
+        if(right != null){
+            size += right.size();
+        }
+        return size + 1;
+    }
+}
